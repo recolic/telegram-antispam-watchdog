37afd9613591ce62ff451840a81834a31006c8fd
Recolic Keghart <git@me.recolic.net>
2022-11-26 15:53:52 -0800

    init

---
+++ README.md
@@ -0,0 +1,47 @@
+# Telegram AntiSpam Watchdog
+
+阻止Telegram的私聊骚扰.
+
+[For English README, click Here](README_en.md)
+
+## 此程序会做什么
+
+此软件是一个24小时运行在你的VPS, 服务器, 或其他设备的Python脚本.
+
+它会作为一个Telegram客户端登入你的帐号. 一旦收到私聊信息, 它会抢先阻止消息通知并删除消息, 然后回复一个验证问题.
+
+在对方正确回答问题之前, 它的所有消息都将被静音并删除. 如果对方正确回答了验证问题, 那么此对话中的所有消息将被放行.
+
+## 如何安装和运行
+
+首先你需要一个运行Linux或MacOS作业系统的24小时开机的设备(例如VPS或云服务器). 你需要安装**Python3和python-telegram**. 以Ubuntu为例, 你可以使用以下命令进行安装:
+
+```
+sudo apt install python3 python3-pip
+sudo pip3 install python-telegram
+```
+
+然后你需要将此仓库下的`watchdog.py`拷贝到你的设备上, 并修改此文件的顶部的几行内容. 如果你不知道如何获取`api_id`和`api_hash`, 烦请阅读[Telegram官方文档](https://core.telegram.org/api/obtaining_api_id#obtaining-api-id).
+
+```
+YOUR_QUESTION = '在这里写上你想问的验证问题'
+YOUR_ANSWER = '在这里写上正确的答案'
+tg = Telegram(
+    api_id='在这里写上你的api_id',
+    api_hash='在这里写上那一串长长的api_hash',
+    phone='在这里写上你的Telegram帐号的手机号码',
+    database_encryption_key='any_password',
+)
+whitelist_filename = 'whitelisted_chats.log'
+```
+
+然后您只需要24小时运行刚刚放入伺服器的`watchdog.py`, 就可以了. 你也可以配置此文件开机自动启动, 或后台运行, 用任何你认为方便的做法.
+
+## Docker
+
+TODO
+
+## 常见问题
+
+暂时没有问题
+
---
+++ watchdog.py
@@ -0,0 +1,101 @@
+from telegram.client import Telegram
+
+##################### Configuration Begin ######################
+YOUR_QUESTION = '12 + 17 = ?'
+YOUR_ANSWER = '29'
+tg = Telegram(
+    api_id='11111111', # Get api_id and api_hash at my.telegram.org
+    api_hash='11111111111111111111111111111111111111111',
+    phone='+15012212221',
+    database_encryption_key='any_password',
+)
+whitelist_filename = 'whitelisted_chats.log'
+##################### Configuration End ########################
+
+whitelisted_chat_ids = []
+
+def read_whitelist_from_disk(fname):
+    try:
+        with open(fname, 'r') as f:
+            for l in f.read().split('\n'):
+                if l != '':
+                    whitelisted_chat_ids.append(int(l))
+    except FileNotFoundError:
+        pass
+
+def write_whitelist_to_disk(fname):
+    with open(fname, 'w+') as f:
+        f.write('\n'.join([str(i) for i in whitelisted_chat_ids]))
+
+def mark_msg_read(chat_id, msg_id):
+    fn_data = {
+        '@type': 'openChat',
+        'chat_id': chat_id,
+    }
+    tg._tdjson.send(fn_data)
+
+    fn_data = {
+        '@type': 'viewMessages',
+        'chat_id': chat_id,
+        'message_ids': [msg_id],
+        'force_read': True,
+    }
+    tg._tdjson.send(fn_data)
+
+    fn_data = {
+        '@type': 'closeChat',
+        'chat_id': chat_id,
+    }
+    tg._tdjson.send(fn_data)
+
+def new_message_handler(update):
+    chat_id = update['message']['chat_id']
+    msg_id = update['message']['id']
+    message_content = update['message']['content']
+    is_outgoing = update['message']['is_outgoing']
+    message_text = message_content.get('text', {}).get('text', '').lower()
+
+    # This handler will block all message which satisfies ALL of the following condition:
+    # 1. Incoming
+    # 2. Not from group chat (Personal chat)
+    # 3. chat_id is not in whitelist
+    # Maybe we can whitelist sender_id instead of chat_id, but I think it doesn't make a difference.
+
+    if is_outgoing:
+        return
+    if chat_id < 0:
+        return
+    if chat_id in whitelisted_chat_ids:
+        return
+
+    print("DEBUG: Received a new private chat message which needs verification.")
+
+    # Mark as read to suppress the notification.
+    mark_msg_read(chat_id, msg_id)
+
+    if message_content['@type'] == 'messageText' and message_text == YOUR_ANSWER.lower():
+        # Answer is correct: add to whitelist and send hello
+        print("DEBUG: good answer")
+        whitelisted_chat_ids.append(chat_id)
+        write_whitelist_to_disk(whitelist_filename)
+        tg.send_message(chat_id=chat_id, text='You have passed the verification. Thanks.')
+    else:
+        # Answer is not correct: send verification message and delete his message.
+        print("DEBUG: bad answer")
+        tg.send_message(chat_id=chat_id, text='This account is protected by Telegram Antispam WatchDog. Please answer the question to continue: ' + YOUR_QUESTION)
+        tg.delete_messages(chat_id, [msg_id])
+
+if __name__ == "__main__":
+    read_whitelist_from_disk(whitelist_filename)
+    tg.login()
+    
+    # if this is the first run, library needs to preload all chats
+    # otherwise the message will not be sent
+    result = tg.get_chats()
+    result.wait()
+    print("Started Telegram Antispam Watchdog. API test by listing your chats: ", result.update)
+    
+    tg.add_message_handler(new_message_handler)
+    tg.idle()  # blocking waiting for CTRL+C
+    tg.stop()  # you must call `stop` at the end of the script
+
